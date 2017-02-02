9729624b975236e13d28c69f1794c38c3bcc75e1
boxmo <mztbrum@gmail.com>
2017-02-02 21:51:53 -0200

    create orgs route

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -6,4 +6,13 @@ class OrgsController < ApplicationController
     render json: @orgs
   end
 
+  def create
+    @org = Org.new(params_orgs)
+    if @org.save
+      render json: @org, status: :ok
+    else
+      render json: { error: @org.errors.messages }, status: 400
+    end
+  end
+
 end
